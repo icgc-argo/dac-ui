00d5d49213d021b16ae04aeb21bb10c2114ce1bf
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2021-06-02 13:12:54 -0400

    Feat/#11 dashboard cards (#87)
    * 14 - add application page with basic titlebar
    
    * add dashboard index page
    
    * add access box icons
    
    * add accesss box component
    
    * file structure
    
    * file organsiation
    
    * move icons and add typography component
    
    * accessbox styling
    
    * cleanup page flow styling
    
    * remove needless prop
    
    Co-authored-by: Anders Richardsson <2107110+caravinci@users.noreply.github.com>

---
+++ components/pages/Applications/Dashboard/Applications/InProgress.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '..';
+import DashboardCard from '../Card';
+
+const InProgress = () => (
+  <DashboardCard title="inprogress">
+    <div>In Progress applications</div>
+  </DashboardCard>
+);
+
+export default InProgress;
---
+++ components/pages/Applications/Dashboard/Header/AccessBox.tsx
@@ -0,0 +1,80 @@
+import { css } from '@icgc-argo/uikit';
+import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
+import Icon from '@icgc-argo/uikit/Icon';
+import { styled } from '@icgc-argo/uikit';
+import Link from '@icgc-argo/uikit/Link';
+import Image from 'next/image';
+import Typography from '@icgc-argo/uikit/Typography';
+
+const IconWithText = styled('div')`
+  font-size: 11px;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  padding: 18px 23px 18px 16px;
+
+  & img {
+    margin-right: 7px;
+  }
+`;
+
+const getConfig = (hasAccess: boolean): { iconName: string; fontColor: string; iconText: string } =>
+  hasAccess
+    ? {
+        iconName: '/icons-checkmark.svg',
+        fontColor: 'accent1_dark',
+        iconText: 'ICGC DACO Approved!',
+      }
+    : { iconName: '/icons-controlled-data.svg', fontColor: 'primary', iconText: 'No Access' };
+
+const AccessBox = ({ hasAccess = false }: { hasAccess?: boolean }) => {
+  const theme = useTheme();
+
+  const { iconName, iconText, fontColor } = getConfig(hasAccess);
+
+  return (
+    <div
+      css={css`
+        align-self: flex-start;
+        border: 1px solid ${theme.colors.grey_2};
+        border-radius: 8px;
+        display: flex;
+        align-items: center;
+        flex: 0 0 379px;
+      `}
+    >
+      <IconWithText hasAccess={hasAccess}>
+        <img src={iconName} width={40} height={40} />
+        <Typography variant="caption" color={fontColor} bold>
+          {iconText}
+        </Typography>
+      </IconWithText>
+
+      <div
+        css={css`
+          border-left: 1px solid #dcdde1;
+          padding: 8px 27px 8px 16px;
+          margin: 8px 0;
+        `}
+      >
+        <Typography
+          as="span"
+          css={css`
+            line-height: 1.69;
+            font-size: 13px;
+          `}
+        >
+          {hasAccess ? (
+            <>
+              You have access to <Link>ICGC Controlled Data »</Link>
+            </>
+          ) : (
+            'You do not have access to ICGC Controlled Data.'
+          )}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default AccessBox;
