8d5087b3fa33d473a4409a40a27ee1a08f20b094
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-06-03 16:39:17 -0400

    add Application state arg and enum

--- components/ApplicationProgressBar.tsx
+++ components/ApplicationProgressBar.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
-import ProgressBar from '@icgc-argo/uikit';
+import Progress, { ProgressItem } from '@icgc-argo/uikit/Progress';
 
-const ApplicationProgressBar = () => <div>Progress Bar</div>;
+export enum ApplicationState {
+  DRAFT = 'DRAFT',
+  SIGN_AND_SUBMIT = 'SIGN AND SUBMIT',
+  REVIEW = 'REVIEW',
+  REVISIONS_REQUESTED = 'REVISIONS REQUESTED',
+  REJECTED = 'REJECTED',
+  APPROVED = 'APPROVED',
+  RENEWING = 'RENEWING',
+  CLOSED = 'CLOSED',
+  EXPIRED = 'EXPIRED',
+}
+
+const ApplicationProgressBar = ({ state }: { state: ApplicationState }) => {
+  console.log('state', state);
+  return (
+    <div>
+      <Progress>
+        <ProgressItem state="success" text="a" />
+        <ProgressItem state="success" text="a" />
+        <ProgressItem state="success" text="a" />
+      </Progress>
+    </div>
+  );
+};
 
 export default ApplicationProgressBar;
--- stories/ProgressBar.stories.tsx
+++ stories/ProgressBar.stories.tsx
@@ -1,15 +1,22 @@
 import { Meta } from '@storybook/react';
-import ApplicationProgressBar from '../components/ApplicationProgressBar';
+import ApplicationProgressBar, { ApplicationState } from '../components/ApplicationProgressBar';
 import { Story } from '@storybook/react';
 import React from 'react';
+
+const ApplicationsStates = Object.values(ApplicationState);
 
 export default {
   title: 'DACO/ProgressBar',
   component: ApplicationProgressBar,
+  argTypes: {
+    state: {
+      options: ApplicationsStates,
+      control: { type: 'radio' },
+      defaultValue: ApplicationsStates[0],
+    },
+  },
 } as Meta;
 
 const Template: Story<any> = (args) => <ApplicationProgressBar {...args} />;
 
 export const Primary = Template.bind({});
-
-Primary.args = {};
