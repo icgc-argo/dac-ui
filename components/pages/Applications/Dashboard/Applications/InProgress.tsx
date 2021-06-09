84251bf150450d7cb5eca8d24526cb596bab2e94
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-06-09 13:31:09 -0400

    add button config

--- components/pages/Applications/Dashboard/Applications/InProgress.tsx
+++ components/pages/Applications/Dashboard/Applications/InProgress.tsx
@@ -1,9 +1,10 @@
 import Button from '@icgc-argo/uikit/Button';
 import React from 'react';
 import DashboardCard from '../Card';
-import { css } from '@icgc-argo/uikit';
+import { css } from '@emotion/core';
 import Typography from '@icgc-argo/uikit/Typography';
 import ApplicationProgressBar, { ApplicationState } from 'components/pages/ProgressBar';
+import { format as formatDate } from 'date-fns';
 
 type InProgressProps = {
   applicationNumber: string;
@@ -11,33 +12,95 @@ type InProgressProps = {
   state: ApplicationState;
 };
 
-const InProgress = ({
-  applicationNumber = 'DACO-XXXXXX',
-  institution = 'Institution: to be specified',
-  state,
-}: InProgressProps) => (
-  <DashboardCard
-    title={`Application: ${applicationNumber}`}
-    subtitle={institution}
-    info={`Access Expiry: May. 28, 2022`}
-  >
-    <div
-      css={css`
-        padding: 24px;
-      `}
-    >
-      <ApplicationProgressBar state={ApplicationState.DRAFT} />
+const EXPIRY_DATE_FORMAT = 'MMMM. dd, yyyy';
 
-      <Typography variant="data">
-        <div>Status:</div>
-        <div>Last Updated:</div>
-      </Typography>
+const getStatusText = () =>
+  'Approved on May. 28, 2021. You now have access to ICGC Controlled Data.';
 
-      <div>
-        <Button>View Application</Button>
+const initState = { appId: '', state: '', submitterId: '', expiresAtUtc: '' };
+
+const getButtonConfig = (state = ''): { content: string; link: string; icon: string }[] => {
+  switch (state) {
+    case ApplicationState.DRAFT:
+    case ApplicationState.SIGN_AND_SUBMIT:
+    case ApplicationState.REVISIONS_REQUESTED:
+      return [{ content: 'Edit Application', link: '/application', icon: 'edit' }];
+    case ApplicationState.REVIEW:
+    case ApplicationState.REJECTED:
+    // closed after approval
+    case ApplicationState.CLOSED:
+      return [{ content: 'View Application', link: '', icon: 'file' }];
+    case ApplicationState.APPROVED:
+      return [
+        { content: 'View Application', link: '', icon: 'file' },
+        { content: 'Manage Collaborators', link: '', icon: 'user' },
+      ];
+    // closed befoer approval
+    case ApplicationState.CLOSED:
+      [
+        { content: 'View Application', link: '', icon: 'file' },
+        { content: 'Reopen', link: '', icon: 'reset' },
+      ];
+  }
+
+  return [];
+};
+
+const InProgress = ({}) => {
+  const [application, setApplication] = React.useState(initState);
+  React.useEffect(() => {
+    fetch('http://localhost:3004/applications')
+      .then((data) => data.json())
+      //.then((d) => console.log(d))
+      .then((d) => setApplication(d[0]))
+      .catch((e) => console.error(e));
+  }, []);
+
+  const { appId, submitterId, state, expiresAtUtc } = application;
+
+  const expiryDate =
+    expiresAtUtc && `Access Expiry: ${formatDate(new Date(expiresAtUtc), EXPIRY_DATE_FORMAT)}`;
+
+  return (
+    <DashboardCard title={`Application: DACO-${appId}`} subtitle={submitterId} info={expiryDate}>
+      <div
+        css={css`
+          padding: 24px;
+        `}
+      >
+        <ApplicationProgressBar state={state as ApplicationState} />
+
+        <Typography
+          variant="data"
+          as="div"
+          css={css`
+            margin-top: 18px;
+          `}
+        >
+          <div>
+            <b>Status:</b> {getStatusText()}
+          </div>
+          <div>
+            <b>Last Updated:</b> May. 28, 2021 at 10:22 p.m.
+          </div>
+        </Typography>
+
+        <div
+          css={css`
+            margin-top: 18px;
+            display: flex;
+
+            &.appbtn {
+            }
+          `}
+        >
+          {getButtonConfig(state).map(({ content, link }) => (
+            <Button className="app-btn">{content}</Button>
+          ))}
+        </div>
       </div>
-    </div>
-  </DashboardCard>
-);
+    </DashboardCard>
+  );
+};
 
 export default InProgress;
