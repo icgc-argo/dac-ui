02c4a155c6c7fd6a93c9ed730d284c51d44a96de
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-06-10 15:39:20 -0400

    add Application.STATE switch to getStatusText

--- components/pages/Applications/Dashboard/Applications/InProgress/helpers.ts
+++ components/pages/Applications/Dashboard/Applications/InProgress/helpers.ts
@@ -6,20 +6,21 @@ export const getStatusText = (state: ApplicationState, date: string | number | D
   const formattedDate = date ? formatDate(new Date(date), SIMPLE_DATE_FORMAT) : '';
 
   switch (state) {
-    case 1:
+    case ApplicationState.APPROVED:
       return `Approved on ${formattedDate}. You now have access to ICGC Controlled Data.`;
-    case 2:
+    case ApplicationState.SIGN_AND_SUBMIT:
+    case ApplicationState.DRAFT:
       return `Created on ${formattedDate}.`;
-    case 3:
+    case ApplicationState.REVIEW:
       return `Submitted on ${formattedDate}. This application is locked for ICGC DACO review.`;
-    case 4:
+    case ApplicationState.REVISIONS_REQUESTED:
       return `Reopened on ${formattedDate}. Revision details were sent via email.`;
-    case 5:
+    case ApplicationState.REJECTED:
       return `Rejected on ${formattedDate}. This application cannot be reopened, reasons were sent via email.`;
-    case 6:
+    case ApplicationState.CLOSED:
       return `Closed on ${formattedDate}. You can reopen this application at anytime.`;
-    case 7:
-      `Closed on ${formattedDate}.`;
+    case ApplicationState.CLOSED:
+      return `Closed on ${formattedDate}.`;
     default:
       return '';
   }
