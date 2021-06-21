a5ec4f512b27c3b94a850b4918143f260a278ae3
Sam Rich <samrichca@users.noreply.github.com>
2021-06-21 17:23:40 -0400

    PR cleanup

--- components/pages/Applications/Dashboard/Applications/InProgress/ButtonGroup.tsx
+++ components/pages/Applications/Dashboard/Applications/InProgress/ButtonGroup.tsx
@@ -83,7 +83,7 @@ const ButtonGroup = ({ state }: { state: ApplicationState }) => (
       margin-top: 35px;
       display: flex;
 
-      & .action-btns::not(::first-of-type) {
+      & .action-btns::not(::first-child) {
         margin-right: 8px;
       }
     `}
--- components/placeholders/index.tsx
+++ components/placeholders/index.tsx
@@ -1,3 +1,1 @@
 export { default as ContentError } from './ContentError';
-export { default as ContentLoader } from './ContentLoader';
-export { default as DnaLoader } from './DNALoader';
