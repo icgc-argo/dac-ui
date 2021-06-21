fe938333ab60575385edf7f5207a32c4e8bb155b
Ann Catton <anncatton@gmail.com>
2021-06-21 17:26:47 -0400

    Correct font import

--- components/Head.tsx
+++ components/Head.tsx
@@ -5,7 +5,7 @@ const Head = () => {
   return (
     <NextHead>
       <link
-        href={'https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap'}
+        href={'https://fonts.googleapis.com/css?family=Work+Sans:300,400,600&display=swap'}
         rel="stylesheet"
       />
       <link rel="shortcut icon" href="/favicon.ico" />
