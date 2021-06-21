03f193563b5ca8b1a9e776c9e0da4bd2fb2cdc57
Sam Rich <samrichca@users.noreply.github.com>
2021-06-21 16:25:07 -0400

    fix pagination

--- components/pages/Applications/ManageApplications/index.tsx
+++ components/pages/Applications/ManageApplications/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { Col, Container, Row } from 'react-grid-system';
 import { SortedChangeFunction } from 'react-table';
 import pluralize from 'pluralize';
