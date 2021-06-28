d41337e9a8f365955441490545a356db4769329e
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2021-06-28 18:38:41 -0400

    Feat/#20 add collaborator modal (#167)
    * add Collaborator section
    
    * add collab section ui
    
    * add collaborators modal form
    
    * add form validation schema
    
    * styling
    
    * add placeholder
    
    * fix bad rebase
    
    * updade form handling props
    
    * Update components/pages/Applications/ApplicationForm/Forms/Collaborators.tsx
    
    Co-authored-by: Anders Richardsson <2107110+caravinci@users.noreply.github.com>
    
    Co-authored-by: Anders Richardsson <2107110+caravinci@users.noreply.github.com>

--- components/pages/Applications/ApplicationForm/Forms/Collaborators.tsx
+++ components/pages/Applications/ApplicationForm/Forms/Collaborators.tsx
@@ -1,6 +1,6 @@
 import Typography from '@icgc-argo/uikit/Typography';
 import { css } from '@emotion/core';
-import React, { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import Button from '@icgc-argo/uikit/Button';
 import Icon from '@icgc-argo/uikit/Icon';
 import Table from '@icgc-argo/uikit/Table';
@@ -9,6 +9,20 @@ import pluralize from 'pluralize';
 import { isEmpty } from 'lodash';
 import ContentPlaceholder from '@icgc-argo/uikit/ContentPlaceholder';
 import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
+import Modal from '@icgc-argo/uikit/Modal';
+import { ModalPortal } from 'components/Root';
+import FormControl from '@icgc-argo/uikit/form/FormControl';
+import FormHelperText from '@icgc-argo/uikit/form/FormHelperText';
+import Input from '@icgc-argo/uikit/form/Input';
+import InputLabel from '@icgc-argo/uikit/form/InputLabel';
+import Select from '@icgc-argo/uikit/form/Select';
+import { honorificsList } from './constants';
+import DoubleFieldRow from './DoubleFieldRow';
+import { isRequired, useLocalValidation } from './validations';
+import { transformToSelectOptions } from './validations/helpers';
+import RadioCheckboxGroup from '@icgc-argo/uikit/form/RadioCheckboxGroup';
+import FormRadio from '@icgc-argo/uikit/form/FormRadio';
+import { FormSectionValidationState_Applicant, FormFieldValidationTriggerFunction } from './types';
 
 const Actions = () => {
   const theme = useTheme();
@@ -59,7 +73,7 @@ const columns = [
   },
 ];
 
-const mock = [
+const mockCollaborators = [
   {
     positionTitle: 'Authorized Personnel',
     firstName: 'Taylor',
@@ -76,8 +90,24 @@ const mock = [
   },
 ];
 
-const Collaborators = ({ appId }: { appId: string }) => {
-  const [collaborators, setCollaborators] = useState(mock);
+enum CollaboratorType {
+  PERSONNEL = 'PERSONNEL',
+  STUDENT = 'STUDENT',
+}
+
+const Collaborators = ({
+  isSectionDisabled,
+  localState,
+  validateFieldTouched,
+}: {
+  isSectionDisabled: boolean;
+  localState: FormSectionValidationState_Applicant;
+  validateFieldTouched: FormFieldValidationTriggerFunction;
+}): ReactElement => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [collaboratorType, setCollaboratorType] = useState(CollaboratorType.PERSONNEL);
+
+  const dismissModal = () => setModalVisible(false);
 
   const containerRef = React.createRef<HTMLDivElement>();
   const theme = useTheme();
@@ -120,8 +150,8 @@ const Collaborators = ({ appId }: { appId: string }) => {
           `}
         >
           <Typography variant="data">
-            {collaborators.length.toLocaleString()}{' '}
-            {pluralize('Collaborators', collaborators.length)}
+            {mockCollaborators.length.toLocaleString()}{' '}
+            {pluralize('Collaborators', mockCollaborators.length)}
           </Typography>
           <Button
             size="sm"
@@ -129,6 +159,7 @@ const Collaborators = ({ appId }: { appId: string }) => {
               display: flex;
               align-items: center;
             `}
+            onClick={() => setModalVisible(true)}
           >
             <Icon
               name="plus_circle"
@@ -149,7 +180,7 @@ const Collaborators = ({ appId }: { appId: string }) => {
           `}
         >
           <Col>
-            {isEmpty(collaborators) ? (
+            {isEmpty(mockCollaborators) ? (
               <ContentPlaceholder
                 title="You have not added any Collaborators."
                 subtitle='To get started, click the "Add a collaborator" button above.'
@@ -172,7 +203,7 @@ const Collaborators = ({ appId }: { appId: string }) => {
                 showPagination={false}
                 defaultSorted={[{ id: 'positionTitle', desc: false }]}
                 columns={columns}
-                data={collaborators}
+                data={mockCollaborators}
                 parentRef={containerRef}
                 stripped
                 withOutsideBorder
@@ -181,6 +212,335 @@ const Collaborators = ({ appId }: { appId: string }) => {
           </Col>
         </Row>
       </section>
+      {isModalVisible && (
+        <ModalPortal>
+          <Modal
+            title="Add a Collaborator"
+            onCancelClick={dismissModal}
+            onCloseClick={dismissModal}
+            actionButtonText="Add Collaborator"
+          >
+            <article
+              css={css`
+                [class*='FormControl'] {
+                  width: 100%;
+
+                  > label {
+                    line-height: 1rem;
+
+                    & ~ p {
+                      margin-top: 5px 0 0;
+                    }
+                  }
+
+                  &.vertical {
+                    margin-bottom: 20px;
+                  }
+
+                  &:not(.vertical) {
+                    align-items: center;
+                    display: flex;
+                    flex-wrap: wrap;
+
+                    > label {
+                      margin: 0 5px 0 0;
+                      flex-shrink: 0;
+                      width: 140px;
+
+                      & ~ p {
+                        margin-left: 150px;
+                      }
+                    }
+
+                    > div {
+                      flex-grow: 1;
+                    }
+
+                    > p {
+                      flex-basis: 100%;
+                    }
+
+                    + p {
+                      margin-top: 30px;
+                    }
+                  }
+
+                  textarea {
+                    font-size: 12px;
+                    line-height: 13px;
+                  }
+                }
+
+                [class*='DoubleFieldRow'] {
+                  margin-bottom: 10px;
+
+                  [class*='FormControl'] {
+                    margin-bottom: 0;
+                  }
+                }
+              `}
+            >
+              <section>
+                <Typography>
+                  Please fill out the following information for the collaborator, including a valid
+                  Google email address that they will use to log in to ICGC ARGO and ICGC 25K and
+                  will be the email address associated with ICGC Controlled Data access.
+                </Typography>
+              </section>
+
+              <section>
+                <FormControl
+                  error={!!localState.info_collaboratorType?.error}
+                  required={isRequired(localState.info_collaboratorType)}
+                >
+                  <InputLabel htmlFor="info_collaboratorType">Collaborator Type</InputLabel>
+                  <RadioCheckboxGroup
+                    id="info_collaboratorType"
+                    aria-label="Collaborator Type"
+                    isChecked={false}
+                    onChange={(type) => setCollaboratorType(type)}
+                    css={css`
+                      display: flex;
+                      > * {
+                        flex: 1;
+                      }
+
+                      > :not(:first-child) {
+                        margin-left: 15px;
+                      }
+                    `}
+                  >
+                    <FormRadio value={CollaboratorType.PERSONNEL}>Authorized Personnel</FormRadio>
+                    <FormRadio value={CollaboratorType.STUDENT}>Authorized Student</FormRadio>
+                  </RadioCheckboxGroup>
+
+                  <FormHelperText onErrorOnly>
+                    {localState.info_collaboratorType?.error?.[0]}
+                  </FormHelperText>
+                </FormControl>
+                <DoubleFieldRow>
+                  <FormControl
+                    error={!!localState.info_title?.error}
+                    required={isRequired(localState.info_title)}
+                  >
+                    <InputLabel htmlFor="info_title">Title</InputLabel>
+
+                    <Select
+                      aria-label="Title"
+                      disabled={isSectionDisabled}
+                      id="info_title"
+                      onBlur={validateFieldTouched}
+                      eventOnChange={validateFieldTouched}
+                      options={transformToSelectOptions(honorificsList)}
+                      value={localState.info_title?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>{localState.info_title?.error?.[0]}</FormHelperText>
+                  </FormControl>
+                  &nbsp;
+                </DoubleFieldRow>
+
+                <DoubleFieldRow>
+                  <FormControl
+                    error={!!localState.info_firstName?.error}
+                    required={isRequired(localState.info_firstName)}
+                  >
+                    <InputLabel htmlFor="info_firstName">First Name</InputLabel>
+
+                    <Input
+                      aria-label="First Name"
+                      disabled={isSectionDisabled}
+                      id="info_firstName"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_firstName?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_firstName?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+
+                  <FormControl
+                    error={!!localState.info_middleName?.error}
+                    required={isRequired(localState.info_middleName)}
+                  >
+                    <InputLabel htmlFor="info_middleName">Middle Name</InputLabel>
+
+                    <Input
+                      aria-label="Middle Name"
+                      disabled={isSectionDisabled}
+                      id="info_middleName"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_middleName?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_middleName?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+                </DoubleFieldRow>
+
+                <DoubleFieldRow>
+                  <FormControl
+                    error={!!localState.info_lastName?.error}
+                    required={isRequired(localState.info_lastName)}
+                  >
+                    <InputLabel htmlFor="info_lastName">Last Name</InputLabel>
+
+                    <Input
+                      aria-label="Last Name"
+                      disabled={isSectionDisabled}
+                      id="info_lastName"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_lastName?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_lastName?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+
+                  <FormControl
+                    error={!!localState.info_suffix?.error}
+                    required={isRequired(localState.info_suffix)}
+                  >
+                    <InputLabel htmlFor="info_suffix">Suffix</InputLabel>
+
+                    <Input
+                      aria-label="Suffix, e.g. Jr., Sr., MD."
+                      disabled={isSectionDisabled}
+                      id="info_suffix"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      placeholder="e.g. Jr., Sr., MD."
+                      value={localState.info_suffix?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_suffix?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+                </DoubleFieldRow>
+
+                <DoubleFieldRow helpText="The legal entity responsible for this application.">
+                  <FormControl
+                    error={!!localState.info_primaryAffiliation?.error}
+                    required={isRequired(localState.info_primaryAffiliation)}
+                  >
+                    <InputLabel htmlFor="info_primaryAffiliation">Primary Affiliation</InputLabel>
+
+                    <Input
+                      aria-label="Primary Affiliation"
+                      disabled={isSectionDisabled}
+                      id="info_primaryAffiliation"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_primaryAffiliation?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_primaryAffiliation?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+                </DoubleFieldRow>
+
+                <DoubleFieldRow helpText="Must be the institutional email address of the Principal Investigator.">
+                  <FormControl
+                    error={!!localState.info_institutionEmail?.error}
+                    required={isRequired(localState.info_institutionEmail)}
+                  >
+                    <InputLabel htmlFor="info_institutionEmail">Institutional Email</InputLabel>
+
+                    <Input
+                      aria-label="Institutional Email"
+                      disabled={isSectionDisabled}
+                      id="info_institutionEmail"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_institutionEmail?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_institutionEmail?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+                </DoubleFieldRow>
+
+                <DoubleFieldRow helpText="Must be the Gmail or G Suite email address of the Principal Investigator.">
+                  <FormControl
+                    error={!!localState.info_googleEmail?.error}
+                    required={isRequired(localState.info_googleEmail)}
+                  >
+                    <InputLabel htmlFor="info_googleEmail">Google Email</InputLabel>
+
+                    <Input
+                      aria-label="Google Email"
+                      disabled={isSectionDisabled}
+                      id="info_googleEmail"
+                      onBlur={validateFieldTouched}
+                      onChange={validateFieldTouched}
+                      value={localState.info_googleEmail?.value}
+                    />
+
+                    <FormHelperText onErrorOnly>
+                      {localState.info_googleEmail?.error?.[0]}
+                    </FormHelperText>
+                  </FormControl>
+                </DoubleFieldRow>
+                <DoubleFieldRow>
+                  {collaboratorType === CollaboratorType.PERSONNEL ? (
+                    <FormControl
+                      error={!!localState.info_positionTitle?.error}
+                      required={isRequired(localState.info_positionTitle)}
+                    >
+                      <InputLabel htmlFor="info_positionTitle">Position Title</InputLabel>
+
+                      <Input
+                        aria-label="Position Title"
+                        disabled={isSectionDisabled}
+                        id="info_positionTitle"
+                        onBlur={validateFieldTouched}
+                        onChange={validateFieldTouched}
+                        value={localState.info_positionTitle?.value}
+                        placeholder="e.g. Bioinformatician"
+                      />
+
+                      <FormHelperText onErrorOnly>
+                        {localState.info_positionTitle?.error?.[0]}
+                      </FormHelperText>
+                    </FormControl>
+                  ) : collaboratorType === CollaboratorType.STUDENT ? (
+                    <FormControl
+                      error={!!localState.info_pursuingDegree?.error}
+                      required={isRequired(localState.info_pursuingDegree)}
+                    >
+                      <InputLabel htmlFor="info_pursuingDegree">Pursuing Degree</InputLabel>
+
+                      <Input
+                        aria-label="Pursuing Degree"
+                        disabled={isSectionDisabled}
+                        id="info_pursuingDegree"
+                        onBlur={validateFieldTouched}
+                        onChange={validateFieldTouched}
+                        value={localState.info_pursuingDegree?.value}
+                        placeholder="e.g. Doctoral"
+                      />
+
+                      <FormHelperText onErrorOnly>
+                        {localState.info_pursuingDegree?.error?.[0]}
+                      </FormHelperText>
+                    </FormControl>
+                  ) : null}
+                  <div />
+                </DoubleFieldRow>
+              </section>
+            </article>
+          </Modal>
+        </ModalPortal>
+      )}
     </article>
   );
 };
