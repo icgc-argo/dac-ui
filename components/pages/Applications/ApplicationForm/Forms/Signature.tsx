99a8dcc4a58e840daec3e0aae39daaf63f1ba59a
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-06-29 10:11:51 -0400

    add Signature section component

---
+++ components/pages/Applications/ApplicationForm/Forms/Signature.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+const Signature = () => {
+  return (
+    <div>
+      <article>
+        <section>
+          The final step involves adding the proper signatures to authorize this application. Please
+          do the following: 1. Download the finalized application: 2. On the last page of the
+          application pdf you will find a signatures page. You must include BOTH the Principal
+          Investigator and the Institutional Representative signatures or your application will be
+          declined. a) You can print this page, collect the written signatures, scan the signed page
+          and add it back to the finalized application pdf. b) Or you can add the proper signatures
+          using electronic methods, such as DocuSign or AdobeSign. 4. Upload the signed application
+          below.
+        </section>
+      </article>
+    </div>
+  );
+};
+
+export default Signature;
