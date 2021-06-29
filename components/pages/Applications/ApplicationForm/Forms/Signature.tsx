cf9794b5dccd767b2e515be285d649b686791ba8
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-06-29 13:31:39 -0400

    styling

--- components/pages/Applications/ApplicationForm/Forms/Signature.tsx
+++ components/pages/Applications/ApplicationForm/Forms/Signature.tsx
@@ -1,20 +1,128 @@
-import React from 'react';
-const Signature = () => {
+import { css } from '@emotion/core';
+import Button from '@icgc-argo/uikit/Button';
+import Icon from '@icgc-argo/uikit/Icon';
+import { useTheme } from '@icgc-argo/uikit/ThemeProvider';
+import Typography from '@icgc-argo/uikit/Typography';
+import { ReactElement } from 'react';
+import { RequiredStar } from './RequiredFieldsMessage';
+
+const Signature = (): ReactElement => {
+  const theme = useTheme();
   return (
-    <div>
-      <article>
-        <section>
-          The final step involves adding the proper signatures to authorize this application. Please
-          do the following: 1. Download the finalized application: 2. On the last page of the
-          application pdf you will find a signatures page. You must include BOTH the Principal
-          Investigator and the Institutional Representative signatures or your application will be
-          declined. a) You can print this page, collect the written signatures, scan the signed page
-          and add it back to the finalized application pdf. b) Or you can add the proper signatures
-          using electronic methods, such as DocuSign or AdobeSign. 4. Upload the signed application
-          below.
-        </section>
-      </article>
-    </div>
+    <article>
+      <Typography bold component="h2">
+        Sign & Submit
+      </Typography>
+      <section
+        css={css`
+          ol {
+            margin: 0;
+            padding: 8px 0 40px 17px;
+          }
+
+          li {
+            line-height: 24px;
+            margin: 25px 0;
+          }
+        `}
+      >
+        <Typography>
+          <p css={css``}>
+            The final step involves adding the proper signatures to authorize this application.
+            Please do the following:{' '}
+          </p>
+          <ol>
+            <li>
+              <div
+                css={css`
+                  display: flex;
+                  align-items: center;
+                `}
+              >
+                {' '}
+                Download the finalized application:{' '}
+                <Button
+                  size="sm"
+                  css={css`
+                    display: inline-block;
+                    line-height: 12px;
+                    margin-left: 6px;
+                  `}
+                >
+                  <span
+                    css={css`
+                      display: flex;
+                      flex-direction: row;
+                      align-items: center;
+                    `}
+                  >
+                    <Icon
+                      css={css`
+                        margin-bottom: -2px;
+                        margin-right: 4px;
+                      `}
+                      fill={theme.colors.white}
+                      height="12px"
+                      name="download"
+                    />
+                    Finalized pdf
+                  </span>
+                </Button>
+              </div>
+            </li>
+            <li>
+              {' '}
+              On the last page of the application pdf you will find a signatures page.{' '}
+              <b>
+                You must include BOTH the Principal Investigator and the Institutional
+                Representative signatures or your application will be declined.
+              </b>
+              <br />
+              <div
+                css={css`
+                  margin-left: 10px;
+                `}
+              >
+                a) You can print this page, collect the written signatures, scan the signed page and
+                add it back to the finalized application pdf. <br />
+                b) Or you can add the proper signatures using electronic methods, such as{' '}
+                <a href="https://www.docusign.ca/">DocuSign</a> or{' '}
+                <a href="https://acrobat.adobe.com/us/en/sign.html">AdobeSign.</a>
+              </div>
+            </li>
+            <li>Upload the signed application below.</li>
+          </ol>
+          <div
+            css={css`
+              display: flex;
+              justify-content: flex-end;
+            `}
+          >
+            <Typography
+              bold
+              variant="label"
+              css={css`
+                margin: 8px 0;
+              `}
+            >
+              <RequiredStar /> Indicates required fields
+            </Typography>
+          </div>
+        </Typography>
+      </section>
+
+      <section>
+        <Typography
+          bold
+          component="h3"
+          css={css`
+            color: #0774d3;
+          `}
+        >
+          UPLOAD SIGNED APPLICATION
+        </Typography>
+      </section>
+    </article>
   );
 };
 
