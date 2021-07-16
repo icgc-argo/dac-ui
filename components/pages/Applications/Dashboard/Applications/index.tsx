8733e44c1d950ca01e55a8661be71eeee64bffc8
Sam Rich <samrichca@users.noreply.github.com>
2021-07-16 14:32:40 -0400

    changes from ciaran's PR

--- components/pages/Applications/Dashboard/Applications/index.tsx
+++ components/pages/Applications/Dashboard/Applications/index.tsx
@@ -2,53 +2,33 @@ import { useEffect, useState } from 'react';
 import InProgress from './InProgress';
 import StartApplication from './Start';
 import { css } from '@emotion/core';
-import { useGetApplications } from 'global/hooks';
 import { ApplicationsResponseItem } from 'components/pages/Applications/types';
-import { ContentError } from 'components/placeholders';
-import Loader from 'components/Loader';
+import { isEmpty } from 'lodash';
 
-const Applications = () => {
-  const [userApplications, setUserApplications] = useState<ApplicationsResponseItem[]>([]);
+const Applications = ({ inProgressApplications }: { inProgressApplications: ApplicationsResponseItem[] }) => {
 
-  const { error, isLoading, response } = useGetApplications();
-
-  useEffect(() => {
-    if (response?.data.items) {
-      setUserApplications(response.data.items);
-    }
-  }, [response])
-
-  return isLoading
-    ? (
-      <div css={css`width: 100%;`}>
-        <Loader css={css`
-          margin: 24px auto;
+  return (
+    <div
+      css={css`
+        flex: 1 0 auto;
+      `}
+    >
+      <div
+        css={css`
+          display: grid;
+          grid-gap: 24px;
+          grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+          margin-bottom: 24px;
         `}
-        />
-      </div>)
-    : error
-      ? <ContentError />
-      : (
-        <div
-          css={css`
-            flex: 1 0 auto;
-          `}
-        >
-          <div
-            css={css`
-              display: grid;
-              grid-gap: 24px;
-              grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-              margin-bottom: 24px;
-            `}
-          >
-            {userApplications.map((application: ApplicationsResponseItem) => (
-              <InProgress application={application} key={application.appId} />
-            ))}
-            <StartApplication />
-          </div>
-        </div>
-      );
+      >
+        {!isEmpty(inProgressApplications) &&
+          inProgressApplications.map((application: ApplicationsResponseItem) => (
+            <InProgress application={application} key={application.appId} />
+          ))}
+        <StartApplication />
+      </div>
+    </div>
+  );
 };
 
 export default Applications;
--- components/pages/Applications/Dashboard/index.tsx
+++ components/pages/Applications/Dashboard/index.tsx
@@ -4,8 +4,15 @@ import PageHeader from 'components/PageHeader';
 import { Container, Row } from 'react-grid-system';
 import Applications from './Applications';
 import AccessBox from './AccessBox';
+import { useGetApplications } from 'global/hooks';
+import { ContentError } from 'components/placeholders';
+import Loader from 'components/Loader';
 
 const Dashboard = () => {
+  const { error, isLoading, response } = useGetApplications();
+
+  const applications = response?.data?.items || [];
+
   return (
     <>
       <PageHeader>My Applications</PageHeader>
@@ -14,36 +21,50 @@ const Dashboard = () => {
           margin-top: 24px;
         `}
       >
-        <Row
-          css={css`
-            justify-content: space-between;
-            margin-bottom: 57px;
-          `}
-          nogutter
-        >
-          <div
-            css={css`
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-            `}
-          >
-            <Typography
-              variant="paragraph2"
-              css={css`
-                margin-right: 80px;
+        {isLoading
+          ? (
+            <div css={css`width: 100%;`}>
+              <Loader css={css`
+                margin: 24px auto;
               `}
-            >
-              This is where you can manage your Applications for Access to ICGC Controlled Data.
-              Access will be granted for a <b>one year period</b>, starting from the date of
-              approval by the ICGC DACO.
-            </Typography>
-            <AccessBox />
-          </div>
-        </Row>
-        <Row nogutter>
-          <Applications />
-        </Row>
+              />
+            </div>)
+          : error
+            ? <ContentError />
+            : (
+              <>
+                <Row
+                  css={css`
+                    justify-content: space-between;
+                    margin-bottom: 57px;
+                  `}
+                  nogutter
+                >
+                  <div
+                    css={css`
+                      display: flex;
+                      justify-content: space-between;
+                      align-items: center;
+                    `}
+                  >
+                    <Typography
+                      variant="paragraph2"
+                      css={css`
+                        margin-right: 80px;
+                      `}
+                    >
+                      This is where you can manage your Applications for Access to ICGC Controlled Data.
+                      Access will be granted for a <b>one year period</b>, starting from the date of
+                      approval by the ICGC DACO.
+                    </Typography>
+                    <AccessBox />
+                  </div>
+                </Row>
+                <Row nogutter>
+                  <Applications inProgressApplications={applications} />
+                </Row>
+              </>
+            )}
       </Container>
     </>
   );
