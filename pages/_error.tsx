2eda56bb9a19dfc2901373d20a49741ad2c52652
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-08-09 17:20:19 -0400

    add generic error handler

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,11 +1,20 @@
 import GenericError from 'components/pages/Error/Generic';
-import React from 'react';
+import { NextPageContext } from 'next';
+import Page403 from './403';
+import Page404 from './404';
 
-function Error({ statusCode }: { statusCode: string }) {
-  return <GenericError />;
+function Error({ statusCode }: { statusCode: number }) {
+  switch (statusCode) {
+    case 404:
+      return <Page404 />;
+    case 403:
+      return <Page403 />;
+    default:
+      return <GenericError />;
+  }
 }
 
-Error.getInitialProps = ({ res, err }) => {
+Error.getInitialProps = ({ res, err }: NextPageContext) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
