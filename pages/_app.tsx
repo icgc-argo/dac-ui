a9d8ad934e978a2b6755aa129eb8169a87c256af
Sam Rich <samrichca@users.noreply.github.com>
2021-08-10 19:45:13 -0400

    finished _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { NextPageContext } from 'next';
 import { AppContext } from 'next/app';
 import Root from 'components/Root';
 import { PageConfigProps, PageWithConfig } from 'global/utils/pages/types';
-import { EGO_JWT_KEY } from 'global/constants';
+import { EGO_JWT_KEY, LOGGED_IN_PATH } from 'global/constants';
 import { isValidJwt } from 'global/utils/egoTokenUtils';
 import Router from 'next/router';
 import Maintenance from 'components/pages/Error/Maintenance';
@@ -35,28 +35,30 @@ const App = ({
   const [initialJwt, setInitialJwt] = useState<string>('');
   const { NEXT_PUBLIC_MAINTENANCE_MODE_ON } = getConfig();
 
+  const logoutToHomepage = () => {
+    console.log('💀 _app - logout to homepage');
+    setInitialJwt('');
+    localStorage.removeItem(EGO_JWT_KEY);
+    // redirect to logout when token is expired/missing only if user is on a non-public page
+    if (!Component.isPublic) {
+      Router.push({
+        pathname: '/',
+        query: { app_session_expired: true },
+      });
+    }
+  };
+
   useEffect(() => {
-    console.log('🏎 _app - start useEffect - initialJwt:', initialJwt.substring(0, 10));
+    console.log('🏎 _app - start useEffect - initialJwt in state:', initialJwt.slice(-10));
+    if (ctx.asPath?.includes(LOGGED_IN_PATH)) return;
     const egoJwt = localStorage.getItem(EGO_JWT_KEY) || '';
-    console.log('🎟 _app - initial localStorage JWT:', egoJwt.substring(0, 10))
+    console.log('🎟 _app - initial localStorage JWT:', egoJwt.slice(-10));
     if (isValidJwt(egoJwt)) {
-      console.log('✅ _app - initial localStorage JWT is valid:', egoJwt.substring(0, 10));
+      console.log('✅ _app - initial localStorage JWT is valid:', egoJwt.slice(-10));
       setInitialJwt(egoJwt);
     } else if (egoJwt) {
-      console.log('❌ _app - initial localStorage JWT exists but is NOT valid, requesting refresh token:'
-        , egoJwt.substring(0, 10));
-      const forceLogout = () => {
-        console.log('💀 _app - force logout');
-        setInitialJwt('');
-        localStorage.removeItem(EGO_JWT_KEY);
-        if (!Component.isPublic) {
-          Router.push({
-            pathname: '/',
-            query: { app_session_expired: true },
-          });
-        }
-      };
-
+      console.log('⏸ _app - initial localStorage JWT is NOT valid:', egoJwt.slice(-10));
+      // if jwt isn't valid, check for refresh token
       queue.add(() =>
         fetch(refreshUrl, {
           credentials: 'include', // sends refreshId cookie
@@ -67,30 +69,25 @@ const App = ({
           method: 'POST',
         })
           .then(res => res.text())
-          .then(newJwt => {
-            if (isValidJwt(newJwt)) {
-              console.log('🎉 _app - refresh token IS valid:', newJwt.substring(0, 10));
-              setInitialJwt(newJwt);
-              localStorage.setItem(EGO_JWT_KEY, newJwt);
+          .then(refreshedJwt => {
+            if (isValidJwt(refreshedJwt)) {
+              console.log('🎉 _app - refresh token IS valid:', refreshedJwt.slice(-10));
+              setInitialJwt(refreshedJwt);
+              localStorage.setItem(EGO_JWT_KEY, refreshedJwt);
             } else {
-              console.log('💥 _app - refresh token NOT valid:', newJwt.substring(0, 10));
-              forceLogout();
+              console.log('💥 _app - refresh token NOT valid:', refreshedJwt.slice(-10));
+              logoutToHomepage();
             }
           })
           .catch((err) => {
             console.log('🧤 _app - refresh token error:', err);
-            forceLogout();
+            logoutToHomepage();
           })
       );
     } else {
-      console.log('💫 _app - no JWT found in localStorage, redirect to homepage');
-      if (!Component.isPublic) {
-        Router.push({
-          pathname: '/',
-        });
-      }
+      logoutToHomepage();
     }
-  });
+  }); // dependencies currently cause page header to not update
 
   return (
     <Root egoJwt={initialJwt} pageContext={ctx}>
