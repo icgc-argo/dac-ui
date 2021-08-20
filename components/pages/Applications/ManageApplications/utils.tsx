f5df1d4b7ed31bb2dabdba69aa0330fee8150c49
Ciarán Schütte <ciaran.schutte@oicr.on.ca>
2021-08-20 10:30:05 -0400

    sort by appNumber (#385)
    * sort by appNumber
    
    * add client side table sorting with default
    
    * add table default sort
    
    * server side sort
    
    * add visual initial sort for table
    
    * remove unneeded sort function
    
    * fix types

--- components/pages/Applications/ManageApplications/utils.tsx
+++ components/pages/Applications/ManageApplications/utils.tsx
@@ -50,77 +50,75 @@ export const fieldDisplayNames = {
 };
 
 export const formatTableData = (data: ApplicationsResponseItem[]) =>
-  data
-    .map<ApplicationRecord>((datum: ApplicationsResponseItem) => ({
-      appId: datum.appId,
-      institution: datum.applicant.info.primaryAffiliation,
-      applicant: datum.applicant.info.displayName,
-      googleEmail: datum.applicant.info.googleEmail,
-      ethicsLetter: datum.ethics.declaredAsRequired,
-      accessExpiry: datum.expiresAtUtc,
-      lastUpdated: datum.lastUpdatedAtUtc,
-      status: datum.state,
-    }));
+  data.map<ApplicationRecord>((datum: ApplicationsResponseItem) => ({
+    appId: datum.appId,
+    institution: datum.applicant.info.primaryAffiliation,
+    applicant: datum.applicant.info.displayName,
+    googleEmail: datum.applicant.info.googleEmail,
+    ethicsLetter: datum.ethics.declaredAsRequired,
+    accessExpiry: datum.expiresAtUtc,
+    lastUpdated: datum.lastUpdatedAtUtc,
+    status: datum.state,
+  }));
 
 export const tableColumns: TableColumnConfig<ApplicationRecord> & {
   id: ApplicationsField;
 } = [
-    {
-      Header: fieldDisplayNames.appId,
-      id: ApplicationsField.appId,
-      accessor: 'appId',
-      Cell: ({ original }: { original: ApplicationRecord }) => original.appId
-        ? (
-          <Link href={urlJoin(APPLICATIONS_PATH, original.appId)}>{original.appId}</Link>
-        )
+  {
+    Header: fieldDisplayNames.appId,
+    id: ApplicationsField.appId,
+    accessor: 'appId',
+    Cell: ({ original }: { original: ApplicationRecord }) =>
+      original.appId ? (
+        <Link href={urlJoin(APPLICATIONS_PATH, original.appId)}>{original.appId}</Link>
+      ) : null,
+  },
+  {
+    Header: fieldDisplayNames['applicant.info.primaryAffiliation'],
+    id: ApplicationsField['applicant.info.primaryAffiliation'],
+    accessor: 'institution',
+  },
+  {
+    Header: fieldDisplayNames['applicant.info.displayName'],
+    id: ApplicationsField['applicant.info.displayName'],
+    accessor: 'applicant',
+  },
+  {
+    Header: fieldDisplayNames['applicant.info.googleEmail'],
+    id: ApplicationsField['applicant.info.googleEmail'],
+    accessor: 'googleEmail',
+  },
+  {
+    Header: fieldDisplayNames['ethics.declaredAsRequired'],
+    id: ApplicationsField['ethics.declaredAsRequired'],
+    Cell: ({ original }: { original: ApplicationRecord }) => (original.ethicsLetter ? 'Yes' : 'No'),
+  },
+  {
+    Header: fieldDisplayNames.expiresAtUtc,
+    id: ApplicationsField.expiresAtUtc,
+    accessor: 'accessExpiry',
+    Cell: ({ original }: { original: ApplicationRecord }) =>
+      original.accessExpiry
+        ? formatDate(new Date(original.accessExpiry), DATE_RANGE_DISPLAY_FORMAT)
         : null,
-    },
-    {
-      Header: fieldDisplayNames['applicant.info.primaryAffiliation'],
-      id: ApplicationsField['applicant.info.primaryAffiliation'],
-      accessor: 'institution',
-    },
-    {
-      Header: fieldDisplayNames['applicant.info.displayName'],
-      id: ApplicationsField['applicant.info.displayName'],
-      accessor: 'applicant',
-    },
-    {
-      Header: fieldDisplayNames['applicant.info.googleEmail'],
-      id: ApplicationsField['applicant.info.googleEmail'],
-      accessor: 'googleEmail',
-    },
-    {
-      Header: fieldDisplayNames['ethics.declaredAsRequired'],
-      id: ApplicationsField['ethics.declaredAsRequired'],
-      Cell: ({ original }: { original: ApplicationRecord }) => (original.ethicsLetter ? 'Yes' : 'No'),
-    },
-    {
-      Header: fieldDisplayNames.expiresAtUtc,
-      id: ApplicationsField.expiresAtUtc,
-      accessor: 'accessExpiry',
-      Cell: ({ original }: { original: ApplicationRecord }) =>
-        original.accessExpiry
-          ? formatDate(new Date(original.accessExpiry), DATE_RANGE_DISPLAY_FORMAT)
-          : null,
-    },
-    {
-      Header: fieldDisplayNames.lastUpdatedAtUtc,
-      id: ApplicationsField.lastUpdatedAtUtc,
-      accessor: 'lastUpdated',
-      Cell: ({ original }: { original: ApplicationRecord }) =>
-        original.lastUpdated
-          ? formatDate(new Date(original.lastUpdated), DATE_RANGE_DISPLAY_FORMAT)
-          : null,
-    },
-    {
-      Header: fieldDisplayNames.state,
-      id: ApplicationsField.state,
-      accessor: 'status',
-      Cell: ({ original }: { original: ApplicationRecord }) =>
-        startCase(original.status.toLowerCase()),
-    },
-  ];
+  },
+  {
+    Header: fieldDisplayNames.lastUpdatedAtUtc,
+    id: ApplicationsField.lastUpdatedAtUtc,
+    accessor: 'lastUpdated',
+    Cell: ({ original }: { original: ApplicationRecord }) =>
+      original.lastUpdated
+        ? formatDate(new Date(original.lastUpdated), DATE_RANGE_DISPLAY_FORMAT)
+        : null,
+  },
+  {
+    Header: fieldDisplayNames.state,
+    id: ApplicationsField.state,
+    accessor: 'status',
+    Cell: ({ original }: { original: ApplicationRecord }) =>
+      startCase(original.status.toLowerCase()),
+  },
+];
 
 export const DEFAULT_PAGE: number = 0;
 export const DEFAULT_PAGE_SIZE: number = 50;
