8924132227ab51427713ce4803911d9d06acc7cb
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-09-14 14:27:16 -0400

    add modal

--- components/pages/Applications/ApplicationForm/Header/Actions.tsx
+++ components/pages/Applications/ApplicationForm/Header/Actions.tsx
@@ -17,7 +17,7 @@
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import { ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { css } from '@icgc-argo/uikit';
 import Button from '@icgc-argo/uikit/Button';
 import Icon from '@icgc-argo/uikit/Icon';
@@ -30,6 +30,13 @@ import { API } from 'global/constants';
 import { AxiosError } from 'axios';
 import { ApplicationState } from '../../types';
 import { CustomLoadingButton, generatePDFDocument } from '../Forms/common';
+import { ModalPortal } from 'components/Root';
+import Modal from '@icgc-argo/uikit/Modal';
+
+enum VisibleModalOption {
+  NONE = 'NONE',
+  CLOSE_APPLICATION = 'CLOSE APPLICATION',
+}
 
 // EXPIRED and RENEWING handling is tbd, CLOSED excludes Action buttons
 const getPdfButtonText: (state: ApplicationState) => string = (state) => {
@@ -62,89 +69,120 @@ const HeaderActions = ({
   const theme: UikitTheme = useTheme();
   const { fetchWithAuth } = useAuthContext();
   const [pdfIsLoading, setPdfIsLoading] = useState<boolean>(false);
+  const [visibleModal, setVisibleModal] = useState<VisibleModalOption>(VisibleModalOption.NONE);
 
   const pdfButtonText = getPdfButtonText(state);
 
-  return (
-    <section
-      css={css`
-        display: flex;
+  const closeApplicationVisible = [
+    ApplicationState.DRAFT,
+    ApplicationState.SIGN_AND_SUBMIT,
+    ApplicationState.REVISIONS_REQUESTED,
+  ].includes(state);
 
-        *:not(:last-of-type) {
-          margin-right: 5px;
-        }
-      `}
-    >
-      {/*
-      // Commented out for soft launch. User flow is not implemented yet.
-      // Please do not delete. Will be used for hard launch.
-      <Button onClick={function noRefCheck() {}} size="sm" variant="secondary">
-        Close Application
-      </Button>*/}
-      <Button
-        // setting width on button & CustomLoadingButton to prevent resize on loading state
+  return (
+    <>
+      {visibleModal === VisibleModalOption.CLOSE_APPLICATION && (
+        <ModalPortal>
+          <Modal
+            actionButtonText="Yes, close"
+            onActionClick={() => console.log('close')}
+            title="Are you sure you want to close this application?"
+            onCancelClick={() => setVisibleModal(VisibleModalOption.NONE)}
+            onCloseClick={() => setVisibleModal(VisibleModalOption.NONE)}
+          >
+            <p>
+              Are you sure you want to close{' '}
+              <b>Application: DACO-12344 (Ontario Institute for Cancer Research)?</b>
+            </p>
+            <p>
+              <b>This action cannot be undone and you will be unable to reopen this application.</b>
+            </p>
+          </Modal>
+        </ModalPortal>
+      )}
+      <section
         css={css`
-          width: ${PDF_BUTTON_WIDTH}px;
-        `}
-        isAsync
-        Loader={(props: any) => <CustomLoadingButton text={pdfButtonText} {...props} />}
-        variant="secondary"
-        isLoading={pdfIsLoading}
-        onClick={async () => {
-          setPdfIsLoading(true);
-          const isDownloadZip = [ApplicationState.REVIEW, ApplicationState.APPROVED].includes(
-            state,
-          );
-          const downloadUrl = urlJoin(
-            API.APPLICATIONS,
-            appId,
-            isDownloadZip ? API.APP_PACKAGE : '',
-          );
-          const data = await fetchWithAuth({
-            url: downloadUrl,
-            ...(isDownloadZip ? { responseType: 'blob' } : {}),
-          })
-            .then((res: any) => {
-              if (res.data && isDownloadZip) {
-                const downloadUrl = window.URL.createObjectURL(new Blob([res.data]));
-                const filename = res.headers['content-disposition'].split('"')[1];
-                const link = document.createElement('a');
-                link.href = downloadUrl;
-                link.setAttribute('download', filename);
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
-                setPdfIsLoading(false);
-                return {};
-              } else {
-                return res.data;
-              }
-            })
-            .catch((err: AxiosError) => {
-              console.error('Application fetch failed, pdf or zip not generated.', err);
-              setPdfIsLoading(false);
-              return null;
-            });
-          // if data fetch fails, do not proceed to pdf generation
-          if (data && !isDownloadZip) {
-            // reset loading state after generate is done
-            await generatePDFDocument(data);
-            setPdfIsLoading(false);
+          display: flex;
+
+          *:not(:last-of-type) {
+            margin-right: 5px;
           }
-        }}
+        `}
       >
-        <Icon
+        {closeApplicationVisible && (
+          <Button
+            onClick={() => setVisibleModal(VisibleModalOption.CLOSE_APPLICATION)}
+            size="sm"
+            variant="secondary"
+          >
+            Close Application
+          </Button>
+        )}
+        <Button
+          // setting width on button & CustomLoadingButton to prevent resize on loading state
           css={css`
-            margin-bottom: -2px;
+            width: ${PDF_BUTTON_WIDTH}px;
           `}
-          fill={theme.colors.accent2_dark}
-          height="12px"
-          width="12px"
-          name="download"
-        />{' '}
-        {pdfButtonText}
-      </Button>
-    </section>
+          isAsync
+          Loader={(props: any) => <CustomLoadingButton text={pdfButtonText} {...props} />}
+          variant="secondary"
+          isLoading={pdfIsLoading}
+          onClick={async () => {
+            setPdfIsLoading(true);
+            const isDownloadZip = [ApplicationState.REVIEW, ApplicationState.APPROVED].includes(
+              state,
+            );
+            const downloadUrl = urlJoin(
+              API.APPLICATIONS,
+              appId,
+              isDownloadZip ? API.APP_PACKAGE : '',
+            );
+            const data = await fetchWithAuth({
+              url: downloadUrl,
+              ...(isDownloadZip ? { responseType: 'blob' } : {}),
+            })
+              .then((res: any) => {
+                if (res.data && isDownloadZip) {
+                  const downloadUrl = window.URL.createObjectURL(new Blob([res.data]));
+                  const filename = res.headers['content-disposition'].split('"')[1];
+                  const link = document.createElement('a');
+                  link.href = downloadUrl;
+                  link.setAttribute('download', filename);
+                  document.body.appendChild(link);
+                  link.click();
+                  link.remove();
+                  setPdfIsLoading(false);
+                  return {};
+                } else {
+                  return res.data;
+                }
+              })
+              .catch((err: AxiosError) => {
+                console.error('Application fetch failed, pdf or zip not generated.', err);
+                setPdfIsLoading(false);
+                return null;
+              });
+            // if data fetch fails, do not proceed to pdf generation
+            if (data && !isDownloadZip) {
+              // reset loading state after generate is done
+              await generatePDFDocument(data);
+              setPdfIsLoading(false);
+            }
+          }}
+        >
+          <Icon
+            css={css`
+              margin-bottom: -2px;
+            `}
+            fill={theme.colors.accent2_dark}
+            height="12px"
+            width="12px"
+            name="download"
+          />{' '}
+          {pdfButtonText}
+        </Button>
+      </section>
+    </>
   );
 };
 
