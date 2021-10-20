8cb983ec6932851db09d5d5e506cd25fa3cd044f
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-10-19 20:26:55 -0400

    add copyrights

--- components/pages/Applications/ApplicationForm/RequestRevisionsBar/PDFActions.tsx
+++ components/pages/Applications/ApplicationForm/RequestRevisionsBar/PDFActions.tsx
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2021 The Ontario Institute for Cancer Research. All rights reserved
+ *
+ * This program and the accompanying materials are made available under the terms of
+ * the GNU Affero General Public License v3.0. You should have received a copy of the
+ * GNU Affero General Public License along with this program.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import Button from '@icgc-argo/uikit/Button';
 import Icon from '@icgc-argo/uikit/Icon';
 import useTheme from '@icgc-argo/uikit/utils/useTheme';
@@ -7,16 +26,17 @@ import { API } from 'global/constants';
 import { useAuthContext } from 'global/hooks';
 import { instructionBoxButtonContentStyle, instructionBoxButtonIconStyle } from 'global/styles';
 import { useState } from 'react';
+import { ApprovedDoc } from '../../types';
 import { CustomLoadingButton } from '../Forms/common';
 import { DOCUMENT_TYPES } from '../Forms/types';
 
 const PDFActions = ({
   appId,
-  doc,
+  currentDoc,
   setLastUpdated,
 }: {
   appId: string;
-  doc: { approvedAppDocObjId: string };
+  currentDoc: ApprovedDoc;
   setLastUpdated: any;
 }) => {
   const theme = useTheme();
@@ -24,7 +44,7 @@ const PDFActions = ({
 
   const [isDeleting, setIsDeleting] = useState(false);
 
-  const docId = doc?.approvedAppDocObjId;
+  const docId = currentDoc?.approvedAppDocObjId;
 
   const deleteDocument = (docId: string) => {
     if (!docId) {
