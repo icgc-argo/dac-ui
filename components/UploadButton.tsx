05528afaf733fc51c5205f9598ac9c78e970440c
Ciaran Schutte <ciaran.schutte@oicr.on.ca>
2021-10-27 20:24:38 -0400

    reset input value for onchange

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -62,7 +62,6 @@ const UploadButton = ({
 
   const handleFileUpload = (e: any) => {
     const file = e.target.files?.[0];
-
     if (file && validators.every((v: (file: any) => boolean) => v(file))) {
       setUploadError(false);
       setUploadInProgress(true);
@@ -84,6 +83,9 @@ const UploadButton = ({
         .finally(() => {
           setUploadInProgress(false);
         });
+    }
+    if (fileInputRef.current) {
+      fileInputRef.current.value = '';
     }
   };
 
